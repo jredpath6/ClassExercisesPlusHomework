fff0d440d2f7c3b54bbf2bad11088ff652c26b1c
jredpath <jredpath@elon.edu>
2018-02-27 15:51:29 -0500

    i/o class exercise JUnit5 is working

--- ch3decorator/Decorator-Feb22/src/headfirst/decorator/io/LowerCaseInputStreamTest.java
+++ ch3decorator/Decorator-Feb22/src/headfirst/decorator/io/LowerCaseInputStreamTest.java
@@ -1,6 +1,6 @@
 package headfirst.decorator.io;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
@@ -10,10 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.*;
 
 class LowerCaseInputStreamTest {
 
@@ -21,24 +18,22 @@ class LowerCaseInputStreamTest {
 	void testRead() {
 		ByteArrayOutputStream array = new ByteArrayOutputStream();
 		try {
-			InputStream in =
-			          new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("elon462.txt")));
-			int a = in.read();
+			InputStream in = new LowerCaseInputStream(
+					new BufferedInputStream(new FileInputStream("elon462.txt")));
+			int a;
 			while ((a = in.read()) > 0) {
-				array.write((char) a);				
+				array.write((char) a);
 			}
 			String actual = array.toString();
-			String expected = "ebruary 22 is the eighth class on design patterns. we are \r\n" + 
-					"25% done with the course.\r\n" + 
-					"\r\n" + 
-					"we have covered three design patterns: strategy, observer\r\n" + 
-					"and decorator. homework 2 will demonstrate our hands on \r\n" + 
-					"understanding of strategy and observer.\r\n" + 
-					"\r\n" + 
-					"optional homework 3 will demonstrate our hands of understanding of\r\n" + 
-					"decorator and java i/o."+"\r\n";
+			String expected = "february 22 is the eighth class on design patterns. we are \r\n"
+					+ "25% done with the course.\r\n" + "\r\n"
+					+ "we have covered three design patterns: strategy, observer\r\n"
+					+ "and decorator. homework 2 will demonstrate our hands on \r\n"
+					+ "understanding of strategy and observer.\r\n" + "\r\n"
+					+ "optional homework 3 will demonstrate our hands of understanding of\r\n"
+					+ "decorator and java i/o." + "\r\n";
 			assertEquals(expected, actual);
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -50,34 +45,44 @@ class LowerCaseInputStreamTest {
 
 	@Test
 	void testReadByteArrayIntInt() {
-			ByteArrayOutputStream outArray = new ByteArrayOutputStream();
-			byte[] byteArray = new byte[3];
-			try {
-				FileInputStream file = new FileInputStream("elon462.txt");
-				InputStream in = new LowerCaseInputStream(new BufferedInputStream(file));
-
-				int a = in.read(byteArray, 0, 2);
-				while ((a = in.read(byteArray, 0, 2)) >= 0) {
-					outArray.write(a);				
+		ByteArrayOutputStream outArray = new ByteArrayOutputStream();
+		String expected = "february 22 is the eighth class on design patterns. we are \r\n"
+				+ "25% done with the course.\r\n" + "\r\n"
+				+ "we have covered three design patterns: strategy, observer\r\n"
+				+ "and decorator. homework 2 will demonstrate our hands on \r\n"
+				+ "understanding of strategy and observer.\r\n" + "\r\n"
+				+ "optional homework 3 will demonstrate our hands of understanding of\r\n"
+				+ "decorator and java i/o." + "\r\n";
+		String expected1 = "FEBRUARY 22 is the eighth class on design patterns. we are \r\n"
+				+ "25% done with the course.\r\n" + "\r\n"
+				+ "we have covered three design patterns: strategy, observer\r\n"
+				+ "and decorator. homework 2 will demonstrate our hands on \r\n"
+				+ "understanding of strategy and observer.\r\n" + "\r\n"
+				+ "optional homework 3 will demonstrate our hands of understanding of\r\n"
+				+ "decorator and java i/o." + "\r\n";
+		byte[] byteArray = new byte[2];
+		try {
+			// FileInputStream file = new FileInputStream("elon462.txt");
+			// InputStream in = new LowerCaseInputStream(new BufferedInputStream(file));
+			InputStream in = new LowerCaseInputStream(
+					new ByteArrayInputStream(expected1.getBytes()));
+			int a;
+			while ((a = in.read(byteArray, 0, 2)) != -1) {
+				if (a == 2) {
+					outArray.write(byteArray);
+				} else {
+					outArray.write(byteArray[0]);
 				}
-				String actual = outArray.toString();
-				String expected = "ebruary 22 is the eighth class on design patterns. we are \r\n" + 
-						"25% done with the course.\r\n" + 
-						"\r\n" + 
-						"we have covered three design patterns: strategy, observer\r\n" + 
-						"and decorator. homework 2 will demonstrate our hands on \r\n" + 
-						"understanding of strategy and observer.\r\n" + 
-						"\r\n" + 
-						"optional homework 3 will demonstrate our hands of understanding of\r\n" + 
-						"decorator and java i/o."+"\r\n";
-				assertEquals(expected, actual);
 			}
-			catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			String actual = outArray.toString();
+
+			assertEquals(expected, actual);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
+}
