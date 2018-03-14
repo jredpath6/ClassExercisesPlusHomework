9e88a2be9d9d690065b7b502b1f590d155487285
jredpath <jredpath@elon.edu>
2018-03-14 17:45:56 -0400

    Calculator Gui is ready for implementation

--- JavaAppletExercise/src/MyApplet.java
+++ JavaAppletExercise/src/MyApplet.java
@@ -8,76 +8,105 @@ import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
 public class MyApplet extends JApplet {
+	public JPanel createButtons() {
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new GridBagLayout());
+		// FIRST ROW
+		GridBagConstraints c = new GridBagConstraints();
+		c.ipady = 15; // Set universal button size
+		c.ipadx = 5;
+		c.gridy = 0; // First row
+		// add 7 button
+		c.gridx = 0; // First column
+		buttons.add(new JButton("7"), c);
+		// add 8 button
+		c.gridx = 1; // Second column
+		buttons.add(new JButton("8"), c);
+		// add 9 button
+		c.gridx = 2; // Third column
+		buttons.add(new JButton("9"), c);
+		// add / button
+		c.gridx = 3; // Fourth column
+		buttons.add(new JButton("/"), c);
+
+		// SECOND ROW
+		c = new GridBagConstraints(); // new set of row/col constraints
+		c.ipady = 15; // Set universal button size
+		c.ipadx = 5;
+		c.gridy = 1; // Second row
+		// add 4 button
+		c.gridx = 0; // First column
+		buttons.add(new JButton("4"), c);
+		// add 5 button
+		c.gridx = 1; // Second column
+		buttons.add(new JButton("5"), c);
+		// add 6 button
+		c.gridx = 2; // Third column
+		buttons.add(new JButton("6"), c);
+		// add * button
+		c.gridx = 3; // Fourth column
+		buttons.add(new JButton("*"), c);
+
+		// THIRD ROW
+		c = new GridBagConstraints();
+		c.ipady = 15; // Set universal button size
+		c.ipadx = 5;
+		c.gridy = 2; // Third row
+		// add 1 button
+		c.gridx = 0; // First column
+		buttons.add(new JButton("1"), c);
+		// add 5 button
+		c.gridx = 1; // Second column
+		buttons.add(new JButton("5"), c);
+		// add 3 button
+		c.gridx = 2; // Third column
+		buttons.add(new JButton("3"), c);
+		// add - button
+		c.gridx = 3; // Fourth column
+		buttons.add(new JButton("-"), c);
+
+		// FOURTH ROW
+		c = new GridBagConstraints();
+		c.ipady = 15; // Set universal button size
+		c.ipadx = 5;
+		c.gridy = 3; // Fourth row
+		// add 0 button
+		c.gridx = 0; // First column
+		buttons.add(new JButton("0"), c);
+		// add . button
+		c.gridx = 1; // Second column
+		buttons.add(new JButton("."), c);
+		// add = button
+		c.gridx = 2; // Third column
+		buttons.add(new JButton("="), c);
+		// add + button
+		c.gridx = 3; // Fourth column
+		buttons.add(new JButton("+"), c);
+
+		buttons.setVisible(true);
+		return buttons;
+	}
+
+	@Override
 	public void init() {
 		getContentPane().setBackground(Color.LIGHT_GRAY);
-		setLayout(new GridLayout(2, 1));
+		setLayout(new BorderLayout());
 		JPanel holdPanel = new JPanel();
 		JPanel upperTextPanel = new JPanel();
 		JPanel buttonPanel = new JPanel();
 		JPanel lowerTextPanel = new JPanel();
-		
-		upperTextPanel.add(new JTextField("", 20), );
+
+		upperTextPanel.add(new JTextField("", 25));
 		upperTextPanel.setBorder(new LineBorder(Color.black));
-		
-		buttonPanel.setLayout(new GridLayout(1,1));
+
+		buttonPanel.setLayout(new GridLayout(1, 1));
 		buttonPanel.add(createButtons());
-		this.add(upperTextPanel);
-		this.add(buttonPanel);
-		
-	}
-	
-	public JPanel createButtons() {
-		JPanel buttons = new JPanel();
-		buttons.setLayout(new GridBagLayout());
-		//FIRST ROW
-		GridBagConstraints c;
-		//add 7 button: top row, first column
-		c = new GridBagConstraints();
-		c.gridx = 0; //First column
-		c.gridy = 0; //First row
-		buttons.add(new JButton("7"), c);
-		//add 8 button: top row, second column
-		c = new GridBagConstraints();
-		c.gridx = 1; //Second column
-		c.gridy = 0; //First row
-		buttons.add(new JButton("8"), c);
-		//add 9 button: top row, third column
-		c = new GridBagConstraints();
-		c.gridx = 2; //Third column
-		c.gridy = 0; //First row
-		buttons.add(new JButton("9"), c);
-		//add 9 button: top row, fourth column
-		c = new GridBagConstraints();
-		c.gridx = 3; //Fourth column
-		c.gridy = 0; //First row
-		buttons.add(new JButton("/"), c);
-		//SECOND ROW
-		//add 7 button: second row, first column
-		c = new GridBagConstraints();
-		c.gridx = 0; //First column
-		c.gridy = 1; //Second row
-		buttons.add(new JButton("4"), c);
-		//add 8 button: second row, second column
-		c = new GridBagConstraints();
-		c.gridx = 1; //Second column
-		c.gridy = 1; //Second row
-		buttons.add(new JButton("5"), c);
-		//add 9 button: second row, third column
-		c = new GridBagConstraints();
-		c.gridx = 2; //Third column
-		c.gridy = 1; //Second row
-		buttons.add(new JButton("6"), c);
-		//add 9 button: second row, fourth column
-		c = new GridBagConstraints();
-		c.gridx = 3; //Fourth column
-		c.gridy = 1; //Second row
-		buttons.add(new JButton("*"), c);
-		
-		buttons.setVisible(true);
-		return buttons;
+		this.add(upperTextPanel, BorderLayout.PAGE_START);
+		this.add(buttonPanel, BorderLayout.CENTER);
+		this.setSize(325, 225);
+
 	}
 }
